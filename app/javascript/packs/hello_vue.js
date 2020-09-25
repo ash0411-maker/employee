b3abfe93f8b596e183e74aa642e88f848233981a
ash <uetani.0411@gmail.com>
2020-09-25 16:47:02 +0900

    [modified] app/javascript/packs/hello_vue.js

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -5,18 +5,14 @@
 // like app/views/layouts/application.html.erb.
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
-import Vue from 'vue'
-import App from '../app.vue'
+import Vue from 'vue/dist/vue.esm'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    render: h => h(App)
-  }).$mount()
-  document.body.appendChild(app.$el)
-
-  console.log(app)
+const app = new Vue({
+  el: "#hello",
+  data: {
+    message: "ヤッホー"
+  }
 })
-
 
 // The above code uses Vue without the compiler, which means you cannot
 // use Vue to target elements in your existing html templates. You would
